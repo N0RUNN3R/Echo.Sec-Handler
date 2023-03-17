e963c5fe29e81a73250df869455923a86f2018f5
RUNN3R <99295465+N0RUNN3R@users.noreply.github.com>
2023-03-17 20:18:17 +0100

    Update SM.lua

--- addons/SM.lua
+++ addons/SM.lua
@@ -1,25 +1,21 @@
 local bbr=function()return(function()
- local qwr = {
-'general9',
-'default2',
-'default3',
-'general12',
-'general13',
-'general15',
-'general16',
-'general30',
-'general31',
-'general33',
-'general35',
-'general36',
-'general39',
-'general40',
-'general45',
-'general47',
-'general49',
-'general51',
-'general55',
-}
-local function b(c)local d=typeof(c)local e={}if(d=="table")then for c,d in next,c,nil do e[b(c)]=b(d)end;setmetatable(e,b(getmetatable(c)))else e=c end;return e end
-svarsEnv.supportedMissions_Env = b(qwr)
+svarsEnv.supportedMissions_Env['general9'] = true
+svarsEnv.supportedMissions_Env['default2'] = true
+svarsEnv.supportedMissions_Env['default3'] = true
+svarsEnv.supportedMissions_Env['general12'] = true
+svarsEnv.supportedMissions_Env['general13'] = true
+svarsEnv.supportedMissions_Env['general15'] = true
+svarsEnv.supportedMissions_Env['general16'] = true
+svarsEnv.supportedMissions_Env['general30'] = true
+svarsEnv.supportedMissions_Env['general31'] = true
+svarsEnv.supportedMissions_Env['general33'] = true
+svarsEnv.supportedMissions_Env['general35'] = true
+svarsEnv.supportedMissions_Env['general36'] = true
+svarsEnv.supportedMissions_Env['general39'] = true
+svarsEnv.supportedMissions_Env['general40'] = true
+svarsEnv.supportedMissions_Env['general45'] = true
+svarsEnv.supportedMissions_Env['general47'] = true
+svarsEnv.supportedMissions_Env['general49'] = true
+svarsEnv.supportedMissions_Env['general51'] = true
+svarsEnv.supportedMissions_Env['general55'] = true
 end)()end;bbr()
